fa5ce4644826af81ebd8f0d8f399127457cfa12b
Mike Bordelon <374mike@gmail.com>
2016-09-28 17:24:21 -0700

    merging

--- app/router.js
+++ app/router.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 // Layouts
-import MainLayout from './components/layouts/main-layout';
+import MainNavBar from './components/layouts/main-navbar';
 // import SearchLayoutContainer from './components/containers/search-layout-container';
 
 // Pages
 import Home from './components/home';
 import UserListContainer from './components/containers/user-list-container';
-import UserProfileContainer from './components/containers/user-profile-container';
+// import UserProfileContainer from './components/containers/user-profile-container';
 // import WidgetListContainer from './components/containers/widget-list-container';
 // import TodoListContainer from './components/containers/todo-list-container';
 // import StaticContainer from './components/containers/static-container';
 
 export default (
   <Router history={browserHistory}>
-    <Route component={MainLayout}>
-      <Route path="/" component={Home}/>
+      <Route component={MainNavBar}>
+      <Route path='/' component={Home}/>
 
-      <Route path='profile'>
-        <IndexRoute component={UserListContainer}/>
+      <Route path='/profile' component={UserListContainer}>
+
       </Route>
 
     </Route>

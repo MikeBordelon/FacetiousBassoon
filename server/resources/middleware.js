dc8393c5cdb7867ed6e1e78bbec6b79ab4c8b900
Alex Pantelides <alex.pantelides@gmail.com>
2016-10-04 20:04:19 -0700

    add redis connect for sessions, need to check how/ if its working

--- server/resources/middleware.js
+++ server/resources/middleware.js
@@ -4,6 +4,8 @@ var session = require('express-session');
 var passport = require('./passport.js');
 var hotModuleReload = require('./hotModuleReload.js');
 var path = require('path');
+var RedisStore = require('connect-redis')(session);
+var options = {url: 'redis://redisdb:6379/0', logErrors: true};
 
 module.exports = (app, express) => {
   hotModuleReload.useWebpackMiddleware(app);
@@ -12,12 +14,9 @@ module.exports = (app, express) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   // app.use('/api', router); // added /api prefix to distinguish back end routes
-  app.use(session({ secret: 'keyboard cat' }));
+  app.use(session({ secret: 'keyboard cat', store: new RedisStore(options), saveUninitialized: true, resave: false}));
   app.use(passport.initialize());
-  app.use(passport.session({
-    resave: false,
-    saveUninitialized: true
-  }));
+  app.use(passport.session());
   app.use(passport.initialize());
   app.use(express.static(path.join(__dirname, '../app/public')));
 };

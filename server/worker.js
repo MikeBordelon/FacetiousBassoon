d2bfd4940861b8a1066b98ec696e844a57f40283
Dustin Kimmel <dkimmel@gmail.com>
2016-10-07 15:06:18 -0700

    rebase

--- server/worker.js
+++ server/worker.js
@@ -2,11 +2,12 @@ const {User, Challenge, UserChallenges, db, Sequelize} = require('./database/db-
 var request = require('request');
 
 
+// container function for worker to tend challenge statuses and fitbit data
 var tendChallenges = function() {
   var startedChallenges = [];
 
   // look up FitBit steps for newly started challenges
-  var checkFitBitStats = function(challengeId) {
+  var checkFitBitStats = function(challengeId, needsBaseline) {
     
     // get a single challenge by challengeId
     Challenge.findOne({
@@ -38,21 +39,30 @@ var tendChallenges = function() {
             // console.log('FitBit API returned: ', JSON.stringify(body));
             console.log('userID: ' + challengePlusJT.dataValues.fbUserId + ' Total steps: ', JSON.stringify(body.lifetime.tracker.steps)); // distance floors steps caloriesOut
             
-            // 
-            challengePlusJT.dataValues.UserChallenge.update(
+            var updateFields;
+            if (needsBaseline) {
+              updateFields = 
               {
                 goalStart: body.lifetime.tracker.steps,
                 goalCurrent: body.lifetime.tracker.steps
-              }
-            )
+              };
+            } else {
+              updateFields = 
+              {
+                goalCurrent: body.lifetime.tracker.steps
+              };
+            }
+
+            // 
+            challengePlusJT.dataValues.UserChallenge.update(updateFields)
             .then(function(result) {
               console.log(JSON.stringify(result)); 
             })
             .catch(function(err) {
               console.log(JSON.stringify(err));
             });
+            return;
           });
-
           // available fields
           // challengePlusJT.id;
           // challengePlusJT.fbUserId;
@@ -70,85 +80,75 @@ var tendChallenges = function() {
     });
   };
 
+  var updateChallenge = function(newChallenge, updateText) {
+    Challenge.findOne({
+      where: { id: newChallenge.id } 
+    })
+    .then(function(challenge) {
+      // call FitBit helper function based on updateText type
+      var needsBaseline = false;
+      if (updateText === 'started') {
+        needsBaseline = true;
+      }
+      // check FitBit API and update goal fields
+      checkFitBitStats(challenge.id, needsBaseline);
+      
+      challenge.update(
+        {
+          status: updateText,
+        }
+      )
+      .then(function(result) {
+        console.log('Worker - challenge ' + challenge.id + ' status changed to: ', updateText);
+        console.log(JSON.stringify(result));
+        return;
+      })
+      .catch(function(err) {
+        console.log(err); 
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  };
 
   // main worker interval loop
   setInterval(function() {
     // Find and transition all challenges that need to flip from 'new' to 'started'
     Challenge.findAll({
-      where: { status: 'new' }
+      where: {
+        $or: [
+                { status: 'new' }, 
+                { status: 'started' }
+        ]
+      }
     })
     .then(function(newChallenges) {
       // console.log(JSON.stringify(newChallenges));
       newChallenges.forEach(function(newChallenge) {
-        // console.log('newChallenge.id: ', newChallenge.id);
-        // console.log('Date.parse(newChallenge.startDate) >= Date.now(): ', Date.parse(newChallenge.startDate) >= Date.now());
-        if ( Date.parse(newChallenge.startDate) <= Date.now() ) {
-          startedChallenges.push(newChallenge);
-          Challenge.findOne({
-            where: { id: newChallenge.id } 
-          })
-          .then(function(challenge) {
-            checkFitBitStats(challenge.id);
-            challenge.update(
-              {
-                status: 'started',
-              }
-            )
-            .then(function(result) {
-              console.log('Worker - challenges, new->starting: ', JSON.stringify(result));
-            })
-            .catch(function(err) {
-              console.log(err);	
-            });
-          })
-          .catch(function(err) {
-            console.log(err);
-          });
-        } 
-      });
-      startedChallenges.forEach(function(startedChal) {
+        // if challenge is status 'new' but should be 'started'...
+        // console.log('newChallenge.status HERE: ', newChallenge.status);
+        // console.log('newChallenge.status = new: ', newChallenge.status = 'new');
+        // console.log('Date.parse(newChallenge.startDate): ', Date.parse(newChallenge.startDate));
+        // console.log('Date.now(): ', Date.now());
+        // console.log('Date.parse(newChallenge.expirationDate): ', Date.parse(newChallenge.expirationDate));
+        // console.log('entire if statement: ', ( (newChallenge.status = 'new') && ( Date.parse(newChallenge.startDate) >= Date.now() ) && ( Date.now() < Date.parse(newChallenge.expirationDate) ) ));
 
+        if ( (newChallenge.status = 'new') && ( Date.parse(newChallenge.startDate) <= Date.now() ) && ( Date.now() < Date.parse(newChallenge.expirationDate) ) ) {
+          // update the challenge status - new->started
+          console.log('updating challenge: new->started');
+          updateChallenge(newChallenge, 'started');
+        }
+        if ( (newChallenge.status = 'started') && ( Date.parse(newChallenge.expirationDate) >= Date.now() ) ) {
+          // update the challenge status - started->expired
+          updateChallenge(newChallenge, 'expired');
+        }
       });
     })
     .catch(function(err) {
       console.log(err);
     });
 
-    // Find and transition all challenges that need to flip from 'started' to 'expired'
-    Challenge.findAll({
-      where: { status: 'started' }
-    })
-    .then(function(newChallenges) {
-      // console.log(JSON.stringify(newChallenges));
-      newChallenges.forEach(function(newChallenge) {
-        // console.log('newChallenge.id: ', newChallenge.id);
-        // console.log('Date.parse(newChallenge.startDate) >= Date.now(): ', Date.parse(newChallenge.startDate) >= Date.now());
-        if ( Date.parse(newChallenge.expirationDate) <= Date.now() ) {
-          Challenge.findOne({
-            where: { id: newChallenge.id } 
-          })
-          .then(function(challenge) {
-            challenge.update(
-              {
-                status: 'expired',
-              }
-            )
-            .then(function(result) {
-              console.log('Worker - challenges, starting->expired: ', JSON.stringify(result));
-            })
-            .catch(function(err) {
-              console.log(err);	
-            });
-          })
-          .catch(function(err) {
-            console.log(err);
-          });
-        } 
-      });
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
 
   }, 10000); // 10000 every ten seconds
 

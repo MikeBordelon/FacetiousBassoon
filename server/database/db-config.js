ce5dcd14fd0b296548d94e81e23b179a79dcc3bc
Alex Pantelides <alex.pantelides@gmail.com>
2016-10-07 11:03:58 -0700

    Remove friends routes/schema/config

--- server/database/db-config.js
+++ server/database/db-config.js
@@ -7,11 +7,9 @@ const {User} = require('./schemas/UserSchema.js')(db, Sequelize);
 const {Challenge} = require('./schemas/ChallengeSchema.js')(db, Sequelize);
 const {UserChallenges} = require('./schemas/UserChallengeJTSchema.js')(db, Sequelize);
 const {Picture} = require('./schemas/PictureSchema.js')(db, Sequelize);
-const {Friend} = require('./schemas/FriendSchema.js')(db, Sequelize);
 
 
 User.belongsToMany(Challenge, {through: UserChallenges, foreignKey: 'userId', otherKey: 'challengeId'});  
 Challenge.belongsToMany(User, {through: UserChallenges, foreignKey: 'challengeId', otherKey: 'userId'});
-User.belongsToMany(User, {as: 'friends', through: 'friends', otherKey: 'userId', foreignKey: 'friendId' }); //doublecheck
 
 module.exports = {db, User, UserChallenges, Challenge, Sequelize};
